8950618e31a8fa7dbaf8d83df74dee5875fe8c93
gu20190801 <76740832+gu20190801@users.noreply.github.com>
2022-11-17 17:50:41 +0800

    Update test_SIFT.py

--- test_SIFT.py
+++ test_SIFT.py
@@ -1,4 +1,3 @@
-import cv2 as cv
 import os
 import numpy as np
 from flask import Flask, render_template, send_file
@@ -12,32 +11,7 @@ database_dir = "image.orig"
 
 @app.route('/')
 def index():
-	image_name = 'horse.jpg'
-	directory = image_name[0:len(image_name)-4]+"/"
-	static = "static/"
-	parent_dir = "C:/Users/s1410/Downloads/2022-Codes-Python/"
-	image_path = parent_dir+image_name
-	static_path = os.path.join(parent_dir, static)
-
-	isExist = os.path.exists(static_path)
-	if not isExist:
-		os.mkdir(static_path)
-
-	path = os.path.join(static_path, directory)
-	isExist = os.path.exists(path)
-	if not isExist:
-		os.mkdir(path)
-
-	img = cv.imread(image_name)
-	
-	global count
-	os.chdir(path)
-	cv.imwrite(str(count)+'.jpg', img)
-	count+=1
-			
-	imageList = os.listdir(path)
-	imagelist = [directory + image for image in imageList]
-	return render_template("index.html", imagelist=imagelist, count=count)
+	return 'happy world!'
 	
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
