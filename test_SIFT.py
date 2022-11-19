46a49b3f43a3e87a27ed7dc8d573994e68d8aa45
gu20190801 <76740832+gu20190801@users.noreply.github.com>
2022-11-19 15:22:58 +0800

    Update test_SIFT.py

--- test_SIFT.py
+++ test_SIFT.py
@@ -1,5 +1,3 @@
-import os
-import numpy as np
 from flask import Flask, render_template, send_file
 
 count = 0
@@ -10,13 +8,6 @@ database_dir = "image.orig"
 @app.route('/')
 def index():
 	return 'happy world!'
-	
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
 
 if __name__ == '__main__':
       app.run(host='127.0.0.1', port=8000)
